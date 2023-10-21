fe2603d1dd362e295b84f8384a6c436d1c2c3e4b
Logon Aluno <logonrmlocal@fiap.com.br>
2023-10-20 21:29:41 -0300

    aula react 20/10/2023

---
+++ dunder/src/App.js
@@ -0,0 +1,24 @@
+// import react from 'react'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import GlobalStyles from './style/GlobalStyles'
+import Home from './pages/Home'
+import Episodios from './pages/Episodios'
+import Novidades from './pages/Novidades'
+import Personagens from './pages/Personagens'
+
+
+function App() {
+  return (
+    <Router>
+        <GlobalStyles />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/episodios" element={<Episodios />}/>
+          <Route path="/novidades" element={<Novidades />}/>
+          <Route path="/personagens" element={<Personagens />}/>
+        </Routes>
+      </Router>
+  );
+}
+
+export default App;
